f3384c584730d732d019f15d8592bb30cdef17d3
Null <53458032+Anonymoushacker4926@users.noreply.github.com>
2021-12-21 20:30:18 -0500

    Update Windows-MACAddress-Spoofer.bat

--- Windows-MACAddress-Spoofer.bat
+++ Windows-MACAddress-Spoofer.bat
@@ -11,33 +11,36 @@ setlocal EnableDelayedExpansion
 ::-VARIABLES--------------------------------------------------------------------------------------------------------
 
 :: Retrieving GUID/UUID
-for /f "skip=2 tokens=2 delims=," %%a in ('wmic nicconfig where IPEnabled^=True GET SettingID /format:csv') do for %%b in (%%a) do set GUID=%%b
+:: for /f "skip=2 tokens=2 delims=," %%a in ('wmic nicconfig where IPEnabled^=True GET SettingID /format:csv') do for %%b in (%%a) do set GUID=%%b
+for /f %%a in ('echo {4d36e972-e325-11ce-bfc1-08002be10318}') do set GUID=%%a
 
 :: Generate Random MACAddress
-for /f "usebackq" %%a in (`powershell -command [BitConverter]::ToString([BitConverter]::GetBytes((Get-Random -Maximum 0xFFFFFFFFFFFF^)^)^, 0^, 6^).Replace(^':^'^, ^'-^'^)`) do (set "RMAC=%%a")
+for /f "usebackq" %%a in (`powershell -command [BitConverter]::ToString([BitConverter]::GetBytes((Get-Random -Maximum 0xFFFFFFFFFFFF^)^)^, 0^, 6^).Replace(^':^'^, ^'-^'^)`) do set RMAC=%%a
 
 :: Retrieving Current MACAddress
-for /f "skip=5" %%a in ('getmac') do set "MAC=%%a"
+for /f "skip=5" %%a in ('getmac') do set MAC=%%a
 
 :: Retrieving Interface Name
-for /f "skip=2 tokens=3*" %%a in ('netsh interface show interface') do (netsh interface set interface name="%%b" admin=disabled & netsh interface set interface name="%%b" admin=enabled)
+for /f "skip=2 tokens=3*" %%a in ('netsh interface show interface') do (netsh interface set interface name=%%b admin=disabled & netsh interface set interface name=%%b admin=enabled)
 
 ::------------------------------------------------------------------------------------------------------------------
 
 :START
 call :DRAW_LOGO
->nul 2>&1 NET SESSION && (echo  [+] Administrator Privileges Detected.) || (echo  [-] No Administrator Privileges Detected. & echo. & pause & exit)
+NET SESSION && (echo  [+] Administrator Privileges Detected.) || (echo  [-] No Administrator Privileges Detected. & echo. & pause & exit) >nul 2>&1
 
 echo. & echo  [+] Active NIC's : & echo.
 wmic nicconfig where (IPEnabled=True) GET Description,SettingID,MACAddress
-echo. & echo  [+] CURRENT MAC: !MAC! & echo.
+echo. & echo  [+] CURRENT MAC: !MAC! & echo. & echo  [+] SPOOFING... & echo.
 
 ::-Spoofing MACAddress---------------------------------------------------------------------------------------------------------------------------
 
-:: Microsoft Kernel Debug Network Adapter
-REG ADD "HKLM\SYSTEM\ControlSet001\Control\Class\!GUID!\0000" /v "NetworkAddress" /t REG_SZ /d !MAC! /f >nul 2>&1
+ipconfig/release >nul 2>&1
 
-:: Actual NIC
+:: Microsoft Kernel Debug Network Adapter
+REG ADD "HKLM\SYSTEM\ControlSet001\Control\Class\!GUID!\0000" /v "NetworkAddress" /t REG_SZ /d !RMAC! /f >nul 2>&1
+
+:: Actual NIC(s)
 REG ADD "HKLM\SYSTEM\ControlSet001\Control\Class\!GUID!\0001" /v "NetworkAddress" /t REG_SZ /d !RMAC! /f >nul 2>&1
 REG ADD "HKLM\SYSTEM\ControlSet001\Control\Class\!GUID!\0002" /v "NetworkAddress" /t REG_SZ /d !RMAC! /f >nul 2>&1
 
@@ -47,17 +50,13 @@ REG DELETE "HKLM\SYSTEM\ControlSet001\Control\Class\!GUID!\0000" /v "OriginalNet
 REG DELETE "HKLM\SYSTEM\ControlSet001\Control\Class\!GUID!\0001" /v "OriginalNetworkAddress" /f >nul 2>&1
 REG DELETE "HKLM\SYSTEM\ControlSet001\Control\Class\!GUID!\0002" /v "OriginalNetworkAddress" /f >nul 2>&1
 
+ipconfig/renew >nul 2>&1
+
 ::-----------------------------------------------------------------------------------------------------------------------------------------------
 
-echo  [+] SPOOFED MAC: !RMAC! & echo. & echo  [+] Applying... Please wait. & echo.)
-
-echo  [+] DISABLING NIC...
-echo.
+echo  [+] SPOOFED MAC: !MAC!
 taskkill /f /im explorer.exe >nul 2>&1
 explorer.exe >nul 2>&1
-echo  [+] ENABLING NIC...
-echo.
-echo  [+] Completed!
 echo.
 pause
 
