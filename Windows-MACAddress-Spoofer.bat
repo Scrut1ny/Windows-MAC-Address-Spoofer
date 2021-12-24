9f6ab08e846c5b3c71b7ccd82a5f69c5bfa7d22b
0x00 <53458032+Scrut1ny@users.noreply.github.com>
2021-12-24 03:55:22 -0500

    Update Windows-MACAddress-Spoofer.bat

--- Windows-MACAddress-Spoofer.bat
+++ Windows-MACAddress-Spoofer.bat
@@ -1,5 +1,6 @@
 ::--------------------------------------
-:: By: 0x00 / Anonymoushacker4926
+:: By: 0x00 | Scrut1ny
+:: https://github.com/Scrut1ny
 :: Windows MACAddress Spoofer
 :: V5.0
 ::--------------------------------------
@@ -12,65 +13,63 @@ setlocal EnableDelayedExpansion
 
 >nul 2>&1 net sess||(powershell saps '%0'-Verb RunAs&exit /b)
 
-::------------------------------------------------------------------------------------------------------------------
+::--------------------------------------------------------------------------------------------------------
 
 :START
 call :LOGO
 >nul 2>&1 NET SESSION && (echo  [+] Administrator Privileges Detected.) || (echo  [-] No Administrator Privileges Detected. & echo. & pause & exit) >nul 2>&1
 
-call :Retrieve_NIC_Info
+call :NIC_Info
 echo. & echo  [+] CURRENT NIC  : !NIC! & echo.
-call :Retrieve_NIC_Info
+call :NIC_Info
 echo  [+] CURRENT MAC  : !MAC! & echo.
 
-::-Spoofing MACAddress---------------------------------------------------------------------------------------------------------------------------
+:: Spoofing MACAddress -----------------------------------------------------------------------------------
 
-call :Retrieve_NIC_Info
+:: Default Windows Network Adapter - ClassGuid = {4d36e972-e325-11ce-bfc1-08002be10318}
+
+call :NIC_Info
 netsh i set i n="!NIC!" a=d >nul 2>&1
 ipconfig/release >nul 2>&1
 
-:: Microsoft Kernel Debug Network Adapter
-call :RMAC
-call :get_guid !GUID1! folder
-REG ADD "HKLM\SYSTEM\ControlSet001\Control\Class\!folder!\0000" /v "NetworkAddress" /t REG_SZ /d !RMAC! /f >nul 2>&1
-
 :: Actual NIC(s)
-call :RMAC
-call :get_guid !GUID1! folder
+call :Random_MAC
+call :Class_GUID !GUID! folder
 REG ADD "HKLM\SYSTEM\ControlSet001\Control\Class\!folder!\0001" /v "NetworkAddress" /t REG_SZ /d !RMAC! /f >nul 2>&1
-call :RMAC
-call :get_guid !GUID1! folder
+call :Random_MAC
+call :Class_GUID !GUID! folder
 REG ADD "HKLM\SYSTEM\ControlSet001\Control\Class\!folder!\0002" /v "NetworkAddress" /t REG_SZ /d !RMAC! /f >nul 2>&1
 
 :: Deleting OriginalNetworkAddress
-call :get_guid !GUID1! folder
-REG DELETE "HKLM\SYSTEM\ControlSet001\Control\Class\!folder!\0000" /v "OriginalNetworkAddress" /f >nul 2>&1
-call :get_guid !GUID1! folder
+call :Class_GUID !GUID! folder
 REG DELETE "HKLM\SYSTEM\ControlSet001\Control\Class\!folder!\0001" /v "OriginalNetworkAddress" /f >nul 2>&1
-call :get_guid !GUID1! folder
+call :Class_GUID !GUID! folder
 REG DELETE "HKLM\SYSTEM\ControlSet001\Control\Class\!folder!\0002" /v "OriginalNetworkAddress" /f >nul 2>&1
 
-call :Retrieve_NIC_Info
+call :NIC_Info
 netsh i set i n="!NIC!" a=e >nul 2>&1
 ipconfig/renew >nul 2>&1
 
-::-----------------------------------------------------------------------------------------------------------------------------------------------
+::---------------------------------------------------------------------------------------------------------
 
-call :Retrieve_NIC_Info
+call :NIC_Info
 echo  [+] SPOOFED MAC  : !MAC!
 echo. & pause & cls
 
-::-VARIABLES--------------------------------------------------------------------------------------------------------
+:: VARIABLES ----------------------------------------------------------------------------------------------
 
 :MENU
 call :LOGO
 echo   [1] Run again
-echo   [2] Exit
+echo   [2] Restart
+echo   [3] Exit
 echo.
 set /p "choice=>> "
 if "%choice%"=="1" goto :START
-if "%choice%"=="2" exit
-echo Choice "%choice%" isn't a valid option. Please try again. & goto :MENU
+if "%choice%"=="2" shutdown /r
+if "%choice%"=="3" exit
+echo Choice "%choice%" isn't a valid option, please try again. & goto :MENU
+exit /b
 
 :LOGO
 cls
@@ -83,27 +82,18 @@ echo.
 exit /b
 
 :: Generate Random MACAddress - Credit: @prsgroup > https://community.idera.com/database-tools/powershell/powertips/b/tips/posts/creating-random-mac-addresses?CommentId=053f086f-7588-4b14-918b-7429c274671f
-
-:RMAC
+:Random_MAC
 for /f "usebackq" %%a in (`powershell -command [BitConverter]::ToString([BitConverter]::GetBytes((Get-Random -Maximum 0xFFFFFFFFFFFF^)^)^, 0^, 6^).Replace(^':^'^, ^'-^'^)`) do set RMAC=%%a
 exit /b
 
-:: Retrieving current MACAddress, Interface Name, & GUID/UUID
-
-:Retrieve_NIC_Info
-for /f "skip=2 tokens=2,3,4* delims=," %%a in ('"wmic nic where (NETEnabled=True) get GUID,NetconnectionID,MACAddress /format:csv"') do (set "GUID1=%%a" & set "MAC=%%b" & set "NIC=%%c")
+:: Retrieving current MACAddress, Interface Name, NetCfgInstanceId
+:NIC_Info
+for /f "skip=2 tokens=2,3,4* delims=," %%a in ('"wmic nic where (NETEnabled=True) get GUID,NetconnectionID,MACAddress /format:csv"') do (set "GUID=%%a" & set "MAC=%%b" & set "NIC=%%c")
 exit /b
 
-:: Retrieving MACAddress Transport Name
-
-:Transport_Name
-for /f "skip=2 tokens=2 delims=," %%a in ('wmic nicconfig where IPEnabled^=True GET SettingID /format:csv') do for %%b in (%%a) do set GUID=%%b
-exit /b
-
-:: Retrieving current NIC GUID/UUID registry folder - Full code at the bottom
-
-:get_guid
+:: Retrieving "Class GUID"
+:Class_GUID
 for /f "tokens=6 delims=\" %%A in ('reg query "HKLM\SYSTEM\ControlSet001\Control\Class" /f "%~1" /s /t REG_SZ ^| find "Class"') do set "%~2=%%A"
 exit /b
 
-::------------------------------------------------------------------------------------------------------------------
+::---------------------------------------------------------------------------------------------------------
