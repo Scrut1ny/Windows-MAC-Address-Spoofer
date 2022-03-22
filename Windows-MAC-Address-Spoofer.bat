759f39454538528d786f211387ed024f383dd8d7
0x00 <53458032+Scrut1ny@users.noreply.github.com>
2022-03-22 18:13:23 -0400

    Update Windows-MAC-Address-Spoofer.bat

--- Windows-MAC-Address-Spoofer.bat
+++ Windows-MAC-Address-Spoofer.bat
@@ -40,7 +40,7 @@ if !nic_selection! GTR 0 (
 		exit /b
 	)
 )
-cls&echo(&echo [31m  "!nic_selection!" invalid selection.[0m
+cls&echo(&echo [31m  Choice "!nic_selection!": Invalid selection.[0m
 >nul timeout /t 2
 goto :SELECTION
 
@@ -54,15 +54,13 @@ echo   [31m# Current MAC  :[0m !MAC!
 echo(
 >nul 2>&1(
 	netsh i set i !NetworkAdapter! a=d
-	reg delete HKLM\SYSTEM\ControlSet001\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\!Index! /v "OriginalNetworkAddress" /f
-	reg add HKLM\SYSTEM\ControlSet001\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\!Index! /v "NetworkAddress" /t REG_SZ /d "!RMAC!" /f
-	reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit /va /f
-	reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Applets\Regedit /va /f
-	arp -d *
+	reg delete "HKLM\SYSTEM\ControlSet001\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\!Index!" /v "OriginalNetworkAddress" /f
+	reg add "HKLM\SYSTEM\ControlSet001\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\!Index!" /v "NetworkAddress" /t REG_SZ /d "!RMAC!" /f
+	reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit" /va /f
+	reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Applets\Regedit" /va /f
 	netsh i set i !NetworkAdapter! a=e
-	ipconfig/release&ipconfig/renew&ipconfig/flushdns
 )
-call :NIC_Info&echo   [31m# Spoofed MAC  :[0m !RMAC!&echo(&>nul pause&(call :EXITMENU || exit /b)
+call :NIC_Info&echo   [31m# Spoofed MAC  :[0m !RMAC!&echo(&echo   [31m#[0m Press any key to continue...&>nul pause&(call :EXITMENU || exit /b)
 
 :EXITMENU
 cls
@@ -75,7 +73,7 @@ set /p c=".  [35m#[0m "
 if %c%==1 goto :SELECTION
 if %c%==2 shutdown /r
 if %c%==3 exit /b 1
-echo Choice "%c%" isn't a valid option, please try again.&goto :EXITMENU
+cls&echo(&echo   [31mChoice "%c%": Invalid option.[0m&>nul timeout /t 2&goto :EXITMENU
 exit /b
 
 :: Generate Random MAC Address
