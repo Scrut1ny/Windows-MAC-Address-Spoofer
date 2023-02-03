e19c990eec74e012915fd636b1c978f04fd6fcab
0x00 <53458032+Scrut1ny@users.noreply.github.com>
2023-02-03 04:39:41 +0000

    Update Windows-MAC-Address-Spoofer.bat

--- Windows-MAC-Address-Spoofer.bat
+++ Windows-MAC-Address-Spoofer.bat
@@ -14,10 +14,10 @@ mode con:cols=66 lines=25
 
 
 fltmc >nul 2>&1 || (
-    echo(&echo   [33m# Administrator privileges are required.&echo([0m
+    echo( && echo   [33m# Administrator privileges are required. && echo([0m
     PowerShell Start -Verb RunAs '%0' 2> nul || (
         echo   [33m# Right-click on the script and select "Run as administrator".[0m
-        >nul pause&exit 1
+        >nul pause && exit 1
     )
     exit 0
 )
