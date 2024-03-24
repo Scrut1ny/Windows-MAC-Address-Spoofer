7a788f1857cd152b1a9f8c82a703fcf93d94e991
0x00 <53458032+Scrut1ny@users.noreply.github.com>
2024-03-24 06:19:11 +0000

    Add files via upload
    Added requested feature: Custom preset vendors

--- Windows-MAC-Address-Spoofer.ps1
+++ Windows-MAC-Address-Spoofer.ps1
@@ -20,50 +20,124 @@ if (-not $isAdmin) {
 $regPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}"
 
 
-# Function to enumerate available NICs
+# Main selection menu
 function Selection-Menu {
-	Clear-Host; Write-Host "`n  [104;97m[i][0m Input NIC # to modify.`n"
-	
-	$counter = 0
-	$nic = Get-CimInstance Win32_NetworkAdapter | Where-Object {$_.NetConnectionID -ne $null} | ForEach-Object {
-		$counter++
-		Write-Host "  $counter - $($_.NetConnectionID)"
-		$_.NetConnectionID
-	}
+    Clear-Host; Write-Host "`n  [104;97m[i][0m Input NIC # to modify.`n"
+    
+    $counter = 0
+    $nic = Get-CimInstance Win32_NetworkAdapter | Where-Object {$_.NetConnectionID -ne $null} | ForEach-Object {
+        $counter++
+        Write-Host "  $counter - $($_.NetConnectionID)"
+        $_.NetConnectionID
+    }
 
-	Write-Host "`n  [92m99[0m - Revise Networking`n"
-	$nicSelection = Read-Host "  "
-	$nicSelection = [int]$nicSelection
+    Write-Host "`n  [92m99[0m - Revise Networking`n"
+    $nicSelection = Read-Host "  "
+    $nicSelection = [int]$nicSelection
 
-	if ($nicSelection -gt 0 -and $nicSelection -le $nic.Count) {
-		$NetworkAdapter = $nic[$nicSelection - 1]
-		Spoof-MAC
-	} elseif ($nicSelection -eq 99) {
-		Clear-Host; Write-Host "`n  [92m# Revising networking configurations...[0m"
-		{
-			ipconfig /release; arp -d *; ipconfig /renew
-		} *>$null
-		Start-Sleep -Seconds 1
-		Selection-Menu
-	} else {
-		Invalid-Selection
-	}
+    if ($nicSelection -gt 0 -and $nicSelection -le $nic.Count) {
+        $NetworkAdapter = $nic[$nicSelection - 1]
+        Spoof-MAC
+    } elseif ($nicSelection -eq 99) {
+        Clear-Host; Write-Host "`n  [92m# Revising networking configurations...[0m"
+        {
+            ipconfig /release
+            arp -d *
+            ipconfig /renew
+        } *> $null
+        Start-Sleep -Seconds 1
+        Selection-Menu
+    } else {
+        Invalid-Selection
+    }
 }
 
 
 # Function to display methods to modify MAC address
 function Spoof-MAC {
-	$originalMAC = Get-MAC
+    $originalMAC = Get-MAC
 	Clear-Host; Write-Host "`n  [91m# Selected NIC:[0m $NetworkAdapter"
 	Write-Host "`n  [91m1[0m - Randomize MAC Address"
 	Write-Host "`n  [91m2[0m - Customize MAC Address"
-	$choice = Read-Host "`n  "
+    $choice = Read-Host "`n  "
 
-	switch ($choice) {
-		1 { Spoof-Random-MAC }
-		2 { Set-Custom-MAC }
-		default { Invalid-Selection }
+    switch ($choice) {
+        1 { 
+			Clear-Host
+            $useVendorPreset = Read-Host "`n  # Apply custom vendor preset? (Y/N)"
+            if ($useVendorPreset -eq 'Y' -or $useVendorPreset -eq 'y') {
+                Spoof-Vendor-Preset
+            } else {
+                Spoof-Random-MAC
+            }
+        }
+        2 { Set-Custom-MAC }
+        default { Invalid-Selection }
+    }
+}
+
+
+function Spoof-Vendor-Preset {
+    $vendors = @(
+        @{ Name = "Apple, Inc."; Prefix = "001A2B" },
+        @{ Name = "Samsung Electronics Co., Ltd"; Prefix = "00163E" },
+        @{ Name = "Dell Inc."; Prefix = "00155D" },
+        @{ Name = "Cisco Systems, Inc"; Prefix = "001B2F" },
+        @{ Name = "Huawei Technologies Co., Ltd"; Prefix = "001E10" },
+        @{ Name = "Intel Corporate"; Prefix = "001B21" },
+        @{ Name = "LG Electronics (Mobile Communications)"; Prefix = "001C62" },
+        @{ Name = "Hewlett Packard"; Prefix = "001A4B" },
+        @{ Name = "Lenovo Group Limited"; Prefix = "001A6B" },
+        @{ Name = "Sony Group Corporation"; Prefix = "001D0D" }
+    )
+
+	# Display vendor list
+	Clear-Host; Write-Host "`n  [104;97m[i][0m Select a vendor for the MAC address prefix:`n"
+    $vendors | ForEach-Object { $index = [Array]::IndexOf($vendors, $_) + 1; Write-Host "  $index - $($_.Name)" }
+
+	# Get user selection
+    $selectedVendorIndex = Read-Host "`n  #"
+	$selectedVendorIndex = [int]$selectedVendorIndex - 1 # Adjust for zero-based indexing
+
+    if ($selectedVendorIndex -ge 0 -and $selectedVendorIndex -lt $vendors.Length) {
+		$selectedVendor = $vendors[$selectedVendorIndex]
+        $randomMac = $selectedVendor.Prefix + ('{0:X}' -f (Get-Random -Minimum 0 -Maximum 0xFFFFFF)).PadLeft(6, '0')
+    } else {
+        Clear-Host; Write-Host "`n  [101;97m[!][0m Invalid selection, please try again."
+		Start-Sleep -Seconds 3
+        Spoof-Vendor-Preset
+    }
+
+	$nicIndex = Get-NICIndex
+
+	Clear-Host; Write-Host "`n  [91m> Registry Path:[0m $regPath\$nicIndex"
+	Write-Host "`n  [91m> Selected NIC:[0m $NetworkAdapter"
+	Write-Host "`n  [91m> Previous MAC:[0m $originalMAC"
+	Write-Host "`n  [91m> Modified MAC:[0m $randomMac"
+	
+	# Disable NIC, delete OriginalNetworkAddress registry entry, add NetworkAddress registry entry, enable NIC
+	Disable-NetAdapter -InterfaceAlias "$NetworkAdapter" -Confirm:$false
+	$registryPath = "$regPath\$nicIndex"
+
+	if (Test-Path $registryPath) {
+		Remove-ItemProperty -Path "$registryPath" -Name "OriginalNetworkAddress" -ErrorAction SilentlyContinue
+
+		try {
+			Set-ItemProperty -Path "$registryPath" -Name "NetworkAddress" -Value "$randomMac" -Force
+			Restart-Service -Force -Name "winmgmt"
+		} catch {
+			Write-Host "`n  [101;97m[!][0m Error setting registry property: $_"
+		}
+	} else {
+		Write-Host "`n  [101;97m[!][0m Registry path not found: $registryPath"
 	}
+
+	Enable-NetAdapter -InterfaceAlias "$NetworkAdapter" -Confirm:$false
+
+	Write-Host "`n  # Press any key to continue..."
+	$null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+	Exit-Menu
 }
 
 
